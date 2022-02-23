e1d8f85eae45f8fa22ab01edd3e947b19739d7ab
silvio <silviomfonseca9@gmail.com>
2022-02-23 13:34:15 -0300

    Commit

---
+++ cypress/integration/pre-cadastro.spec.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+const { faker } = require('@faker-js/faker');
+
+describe('Funcionalidade Pré Cadastro', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+it('Deve completar o Pré Cadastro com Sucesso', () => {
+    cy.get('#reg_email').type(faker.internet.email())
+    cy.get('#reg_password').type('!teste@teste$')
+    cy.get(':nth-child(4) > .button').click()
+
+    cy.get('.woocommerce-MyAccount-navigation-link--edit-account').click()
+    cy.get('#account_first_name').type ('silvio')
+    cy.get('#account_last_name').type('Fonseca')
+    cy.get('.woocommerce-Button').click
+});
+
+    
+});
+
